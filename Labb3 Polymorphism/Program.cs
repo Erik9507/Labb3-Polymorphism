710cb78e15f52d1497bf720512c988dfff35b614
Erik9507 <eriknorell1995@gmail.com>
2021-11-05 10:46:42 +0100

    Uppdaterad

--- Labb3 Polymorphism/Program.cs
+++ Labb3 Polymorphism/Program.cs
@@ -15,14 +15,6 @@ namespace Labb3_Polymorphism
             Console.WriteLine("Cirkel med en radie på "+ cirkel.Radie + " har en area som är " + cirkel.Area());
             Console.WriteLine("Rektangel med en bas som är " + rektangel.Bas + " och en höjd som är " + rektangel.Höjd + " har en area som är " + rektangel.Area());
             Console.WriteLine("Fyrkant med en sida som är " + fyrkant.Sida + " har en area som är " + fyrkant.Area());
-
-
-            
-
-            
-
         }
     }
-    
-
 }
